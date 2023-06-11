a7f6e4c6616c2ec14c53765f6baa205b28b29869
Fuad Hasan Labib <labib.knc@gmail.com>
2023-06-11 17:58:45 +0600

    Check If It Is a Straight Line

---
+++ CheckIfItIsaStraightLine.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CheckIfItIsaStraightLine {
+    public static void main(String[] args) {
+        int coordinates[][] ={{1,1},{2,2},{2,1},{3,2}};
+        System.out.println(new CheckIfItIsaStraightLine().checkStraightLine(coordinates));
+    }
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x0 = coordinates[0][0], y0 = coordinates[0][1],
+                x1 = coordinates[1][0], y1 = coordinates[1][1];
+        int dx = x1 - x0, dy = y1 - y0;
+        for (int[] co : coordinates) {
+            int x = co[0], y = co[1];
+            if (dx * (y - y1) != dy * (x - x1))
+                return false;
+        }
+        return true;
+    }
+}
